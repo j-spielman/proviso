1dc94d1a0106e4c681ecf794145259e1f7e9e911
Ricardo Guillen <rguillenvergara@my365.bellevue.edu>
2022-02-16 18:25:52 -0800

    new reservation and complete reservation

---
+++ WebContent/reservation/completereservation.jsp
@@ -0,0 +1,117 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+ 
+
+ 
+  <!DOCTYPE html>
+
+<html>
+	<head>
+	<meta charset="UTF-8">
+	<title>Proviso Confirm Reservation</title>
+	</head>
+	
+	<link
+		rel="stylesheet" href="../css/site.css">
+			
+	
+	<body>
+		
+		
+		
+		<!-- 2: Create Form to accept new reservation -->
+   		<h2 style="float:right;font-size:1em;">Welcome -- <% /*session.getAttribute("login")*/ %><a href="/proviso/store/?action=logoutUser">Logout?</a></h2>
+		
+			
+		<!-- Tie in the topNav file and where I want it. -->
+		<jsp:include page="../header.jsp" flush="true" />	
+	
+    
+
+    
+    <%
+    String checkIn = request.getParameter("checkIn");
+    String checkOut = request.getParameter("checkOut");
+    String roomSize = request.getParameter("roomSize");
+	
+	String guests = request.getParameter("guests");
+	Boolean loyalty_points_reservation = Boolean.parseBoolean(request.getParameter("loyalty_points_reservation").trim());
+	String customer_id = request.getParameter("customer");
+	
+	// add 150 points.
+	int pointsToAdd = 0;
+	if(loyalty_points_reservation) {
+		pointsToAdd = 150;
+		System.out.println("Setting points to 150");
+	}
+	System.out.println("Loyalty Points parameter: <"+loyalty_points_reservation+">");
+
+	String wifi = request.getParameter("wifi");
+	String breakfast = request.getParameter("breakfast");
+	String parking = request.getParameter("parking");
+	String amenities = "";
+	if (wifi != null) {
+		amenities += "wifi: yes</br>";
+	}
+	if (breakfast != null) {
+		amenities += "breakfast: yes</br>";
+	}
+	if (parking != null) {
+		amenities += "parking: yes</br>";
+	}
+			
+	dbBeans.Reservation newReservation = new dbBeans.Reservation(); 
+	
+	newReservation.setCheckIn(checkIn);
+	newReservation.setCheckOut(checkOut);
+	newReservation.setRoomSize(roomSize);
+	newReservation.setAmenities(amenities);
+	newReservation.setGuests(guests);
+	newReservation.setloyalty_points_reservation(pointsToAdd);
+	newReservation.setCustomer_id(customer_id);
+	
+
+	dbBeans.JdbcReservationDao reservationDao = new dbBeans.JdbcReservationDao(); 
+	long reservation_id = reservationDao.add(newReservation);
+
+	System.out.println(newReservation.toString());
+	System.out.println(String.format("Reservation{checkIn=%s, checkOut=%s, roomSize=%s, amenities=%s, guests=%s, loyalty_points_reservation=%s, customer_id=%s}", checkIn, checkOut, roomSize, amenities, guests, loyalty_points_reservation, customer_id));
+
+	
+	if (reservation_id > 0){
+		
+		String user = (String) session.getAttribute("login");
+		System.out.println("Adding loyalty points to customer " + user + " number: " + pointsToAdd);
+		if(user!= "") {
+			dbBeans.JdbcCustomerDao customerDao = new dbBeans.JdbcCustomerDao(); 
+			customerDao.addloyalty_points_customer(user,pointsToAdd);
+		}
+	}
+	
+    %>
+
+
+        
+    <!-- 2: Output Summary of Form to confirm new reservation number -->
+    	<h1>RESERVATION CONFIRMED</h1>
+    	
+    	<div class="center-form width-35">
+			Your new reservation confirmation number is: 
+		<%= reservation_id %>
+        </div><br><br><br>
+        
+        <div class="center-form width-35">
+        	<a href="/proviso/store/?action=showloyalty_points_reservation">Click Here</a> if you'd like to view your current loyalty points.
+        </div>
+        
+   		<br><br><br>
+       
+       
+
+    	<!-- Page footer	 -->
+		 <jsp:include page="../footer.jsp" flush="true" />
+	
+		 
+	
+	</body>
+</html>
---
+++ src/dbBeans/JdbcReservationDao.java
@@ -0,0 +1,221 @@
+package dbBeans;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+
+import dbBeans.Reservation;
+import dbBeans.JdbcManager;
+import dbBeans.ReservationDao;
+
+public class JdbcReservationDao implements ReservationDao {
+
+	JdbcManager db; 
+	
+	public JdbcReservationDao() 
+	{
+		// constructor
+		db = new JdbcManager(); 
+	}
+	
+	
+	@Override
+	public long add(Reservation entity) // create
+	{
+		Connection con = db.getConn(); 
+		Reservation newReservation = entity; 
+		
+		if (con != null) 
+		{
+			Statement s;
+			try 
+			{
+				s = con.createStatement(); 	
+				
+				long customer_id = 1;
+				
+				String sql = String.format("INSERT INTO reservation(checkIn, checkOut, roomSize, customer_id, amenities, guests, loyalty_points_reservation)" +
+						" values('%s', '%s', '%s', %s, '%s', '%s', %s);", 
+						newReservation.getCheckIn(), newReservation.getCheckOut(),  						
+						newReservation.getRoomSize(), customer_id, 
+						newReservation.getAmenities(), newReservation.getGuests(), 
+						newReservation.getloyalty_points_reservation());
+				
+				System.out.println(sql);
+				
+				s.executeUpdate(sql);
+				long reservation_id = 0;
+				
+				Statement selectStatement = con.createStatement();
+				
+				ResultSet rs = selectStatement.executeQuery("SELECT MAX(reservation_id) FROM reservation;");
+				rs.next();
+				reservation_id = rs.getLong(1);
+				System.out.println("Created reservation " + reservation_id);
+				
+				s.close();
+				return reservation_id;
+			}
+			catch (SQLException ex)
+			{
+				System.out.println("Sorry, we are unable to insert new reservation: " + newReservation.toString()); 
+				System.out.println(ex.getMessage());
+			}
+		}
+		return 0;
+	}
+
+	@Override
+	public List<Reservation> list() 
+	{
+		Connection con = db.getConn(); 
+		ArrayList<Reservation> reservations = new ArrayList<Reservation>();
+		
+		if (con != null) 
+		{
+			try 
+			{
+				Statement s = con.createStatement();
+				
+				String sql = "select reservation_id, roomSize, customer_id, amenities, guests, loyaltyPoints, checkIn, checkOut from reservation";
+				
+				System.out.println(sql);
+				
+				try 
+				{
+					ResultSet rs = s.executeQuery(sql);
+					
+					try 
+					{
+						while (rs.next()) 
+						{
+							Reservation reservation = new Reservation();
+							reservation.setReservation_id(rs.getLong(1));
+							reservation.setCheckIn(rs.getString(2));
+							reservation.setCheckOut(rs.getString(3));
+							reservation.setRoomSize(rs.getString(4));
+							reservation.setCustomer_id(rs.getString(5));
+							reservation.setamenities(rs.getString(6));
+							reservation.setGuests(rs.getString(7));
+							reservation.setloyalty_points_reservation(rs.getInt(8));
+							reservations.add(reservation);
+						}
+					}
+					finally { rs.close(); }
+				}
+				finally { s.close(); }
+			}
+			catch (SQLException ex)
+			{
+				System.out.println("Sorry, we could not get reservations: " + ex.getMessage()); 
+			}
+			finally
+			{
+				db.closeConn(con);
+			}
+		}
+		
+		return reservations;
+	}
+
+	@Override
+	public Reservation find(Long key) 
+	{
+		Connection con = db.getConn(); 
+		
+		Reservation reservation = null; 
+		
+		if (con != null) 
+		{
+			try 
+			{
+				Statement s = con.createStatement(); 
+				
+				String sql = "select reservation_id, checkIn, checkOut, roomSize, customer_id, amenities, guests, loyalty_points_reservation, checkIn, checkOut from reservation where reservation_id = " + key;
+				
+				System.out.println(sql);
+				
+				try 
+				{
+					ResultSet rs = s.executeQuery(sql);
+					
+					try 
+					{
+						if (rs.next()) 
+						{
+							reservation = new Reservation(rs.getLong(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getString(6), rs.getString(7), rs.getInt(8)); 
+						}
+					}
+					finally { rs.close(); }
+				}
+				finally { s.close(); }
+				
+			}
+			catch (SQLException ex)
+			{
+				System.out.println("Sorry, we could not get reservation: " + ex.getMessage());
+			}
+		}
+		return reservation;
+	}
+
+	@Override
+	public void update(Reservation entity) {
+		Connection con = db.getConn(); 
+		
+		
+		Reservation updatedReservation = entity; 
+		
+		if (con != null) 
+		{
+			try {
+				Statement s = con.createStatement(); 
+				
+				String sql = String.format("UPDATE reservation SET reservation_id = '%s', checkIn = %s, checkOut = %s, roomSize = %s, customer_id = '%s', amenities = '%s', guests = %s WHERE loyalty_points_reservation = %s;", 
+						updatedReservation.getReservation_id(), updatedReservation.getCheckIn(), updatedReservation.getCheckOut(), updatedReservation.getRoomSize(), updatedReservation.getCustomer_id(), updatedReservation.getAmenities(), updatedReservation.getGuests(), updatedReservation.getloyalty_points_reservation());
+				
+				System.out.println(sql);
+				
+				try {
+					s.executeUpdate(sql);
+				} finally { s.close(); }
+			}
+			catch (SQLException ex) {
+				System.out.println("Our Apologies, the reservation was not updated: " + updatedReservation.toString());
+				System.out.println(ex.getMessage());
+			}
+		}
+	}
+
+	@Override
+	public void remove(Long key) {
+		Connection con = db.getConn(); 
+		
+		if (con != null) 
+		{	
+			try 
+			{	
+				Statement s = con.createStatement(); 
+				
+				String sql = String.format("DELETE FROM reservation WHERE reservation_id = %s", key);
+				
+				System.out.println(sql);
+				
+				try 
+				{
+					s.executeUpdate(sql);
+				} 
+				finally { s.close(); }
+			} 
+			catch (SQLException ex) 
+			{
+				System.out.println("Sorry we could not delete reservation: " + key);
+				System.out.println(ex.getMessage());
+			}
+		}
+	}
+}
